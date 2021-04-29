ca89fb14c340a413f7ba367aa10bd0dd24213cba
pekellogg <pekellogg@hotmail.com>
2021-04-28 18:14:29 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.!?] /)
+    sentence_array.count
   end
 end
+
+"This is a string! It has three sentences. Right?".count_sentences
